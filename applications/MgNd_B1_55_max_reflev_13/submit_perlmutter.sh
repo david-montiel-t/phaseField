242ca868ea404717ec57777248768f3a01b3a08d
David Montiel <dmontiel@login35.chn.perlmutter.nersc.gov>
2024-11-11 11:58:17 -0800

    Added app that contains code files- MgNd_B1_55_max_reflev_13

---
+++ applications/MgNd_B1_55_max_reflev_13/submit_perlmutter.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+
+#SBATCH -A m2360
+#SBATCH -J myjob
+#SBATCH -o myjob.o%j
+#SBATCH -e myjob.e%j
+#SBATCH --qos=regular
+#SBATCH --constraint=cpu
+#SBATCH --nodes 4
+#SBATCH --tasks-per-node=64
+#SBATCH -t 24:00:00
+#SBATCH --mail-user=dmontiel@umich.edu
+#SBATCH --mail-type=all
+
+module list
+pwd
+date
+
+#Creating target directory on scratch and copying files into it
+locdir=${PWD##*/}
+targpath=$SCRATCH
+fulltargpath=$targpath/$locdir
+
+echo $fulltargpath
+
+mkdir $fulltargpath
+cp parameters.prm $fulltargpath
+cp main $fulltargpath
+#cp microstructure_US.vtk $fulltargpath
+cd $fulltargpath
+
+srun -n 256 --cpu_bind=cores ./main -i parameters.prm
